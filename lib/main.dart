04e5af63080d96a82ffcc80f49c7448cdb22be54
agora.creation <apple@agora-c.com>
2022-04-20 19:00:21 +0900

    20220420 agora commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:hatarakujikan_tablet/helpers/functions.dart';
 import 'package:hatarakujikan_tablet/helpers/style.dart';
 import 'package:hatarakujikan_tablet/providers/group.dart';
 import 'package:hatarakujikan_tablet/providers/work.dart';
 import 'package:hatarakujikan_tablet/screens/home.dart';
 import 'package:hatarakujikan_tablet/screens/login.dart';
 import 'package:hatarakujikan_tablet/screens/splash.dart';
-import 'package:hatarakujikan_tablet/widgets/custom_text_button.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
@@ -38,9 +36,9 @@ class MyApp extends StatelessWidget {
           GlobalWidgetsLocalizations.delegate,
         ],
         supportedLocales: [
-          const Locale('ja', ''),
+          const Locale('ja'),
         ],
-        locale: const Locale('ja', ''),
+        locale: const Locale('ja'),
         title: 'はたらくじかんforタブレット',
         theme: theme(),
         home: SplashController(),
@@ -55,46 +53,6 @@ class SplashController extends StatefulWidget {
 }
 
 class _SplashControllerState extends State<SplashController> {
-  void _init() async {
-    await versionCheck().then((value) {
-      if (!value) return;
-      showDialog(
-        barrierDismissible: false,
-        context: context,
-        builder: (_) => AlertDialog(
-          title: Text(
-            'アップデートのお知らせ',
-            style: TextStyle(fontSize: 18.0),
-          ),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('各種パフォーマンスの改善および新機能を追加しました。最新バージョンへのアップデートをお願いします。'),
-              SizedBox(height: 16.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  CustomTextButton(
-                    onPressed: () => launchUpdate(),
-                    label: 'アップデート',
-                    color: Colors.blue,
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      );
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _init();
-  }
-
   @override
   Widget build(BuildContext context) {
     final groupProvider = Provider.of<GroupProvider>(context);
